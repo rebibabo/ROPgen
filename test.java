7d7cb74c7e0f1bdc5c40919f55b656bd0ddf707c
rebibabo <2841329980@qq.com>
2023-10-17 15:18:10 +0000

    delete __pycache__ and test.*

--- test.java
+++
@@ -1,9 +0,0 @@
-public class Fibonacci {
-    public static void main(String[] args) {
-        int n = 10; // 生成斐波那契数列的前n个数字
-
-        int[] fibonacciSeries = new int[n];
-        fibonacciSeries[0] = 0;
-        fibonacciSeries[1] = 1;
-    }
-}
